5d71559be47aed4a6535a8dd0195814da1c7bc28
bakajinsei <cqggff@gmail.com>
2018-10-12 17:40:58 +0800

    [-]Remove some log

--- src/server/filters/prepend.ts
+++ src/server/filters/prepend.ts
@@ -2,14 +2,6 @@ import {app, escapeData, filterSensitiveFields, isObjEmpty} from '../modules/uti
 
 // Set error handle
 app.addHook('preHandler', (request, _reply, next) => {
-    if (!isObjEmpty(request.query)) {
-        escapeData(request.query);
-        request.log.info('request query data:', filterSensitiveFields(request.query));
-    }
-    if (!isObjEmpty(request.params)) {
-        escapeData(request.params);
-        request.log.info('request params data:', filterSensitiveFields(request.params));
-    }
     if (!isObjEmpty(request.body)) {
         escapeData(request.body);
         request.log.info('request body data:', filterSensitiveFields(request.body));
